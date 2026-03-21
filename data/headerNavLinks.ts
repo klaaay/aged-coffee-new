a5f32c0fc02e5fc09e403a06395246ebdcd9a45e
klaaay <klaaaywu@163.com>
2026-03-22 02:41:02 +0800

    refactor ai resources into smaller pages

--- data/headerNavLinks.ts
+++ data/headerNavLinks.ts
@@ -2,7 +2,7 @@ const headerNavLinks = [
   { href: '/blog', title: '记录' },
   { href: '/tags', title: '分类' },
   // { href: '/projects', title: 'Projects' },
-  { href: '/blog/resources-save/tech-manual', title: '手册' },
+  { href: '/blog/resources-save/ai-manual', title: '手册' },
   // { href: '/blog/resources-save/tech-note', title: '笔记' },
   { href: '/blog/resources-save/tools-collection', title: '工具' },
   // { href: '/blog/photo-gallery/photos', title: '相册簿' },
