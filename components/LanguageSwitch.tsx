3a98111922ff23f63e47434835cd86397aa69f36
klaaay <klaaaywu@163.com>
2026-07-23 03:09:09 +0800

    Align language switch icon

--- components/LanguageSwitch.tsx
+++ components/LanguageSwitch.tsx
@@ -22,9 +22,23 @@ export default function LanguageSwitch() {
       }}
       aria-label={label}
       title={label}
-      className="hover:text-primary-500 dark:hover:text-primary-400 flex h-7 min-w-7 items-center justify-center rounded px-1 text-xs font-semibold text-gray-900 disabled:opacity-50 dark:text-gray-100"
+      className="hover:text-primary-500 dark:hover:text-primary-400 flex items-center justify-center text-gray-900 disabled:opacity-50 dark:text-gray-100"
     >
-      {locale === 'zh-CN' ? 'EN' : '中'}
+      <svg
+        aria-hidden="true"
+        xmlns="http://www.w3.org/2000/svg"
+        fill="none"
+        viewBox="0 0 24 24"
+        strokeWidth={1.5}
+        stroke="currentColor"
+        className="h-6 w-6"
+      >
+        <path
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          d="M10.5 21l5.25-11.25L21 21m-9.75-3h9M3 5.25h6m-3-1.5v1.5m2.25 0c0 4.142-2.507 7.5-5.625 7.5M3 12.75c1.836 0 3.5-.69 4.713-1.805M3.75 5.25c0 2.036 1.136 3.82 2.842 4.773"
+        />
+      </svg>
     </button>
   )
 }
